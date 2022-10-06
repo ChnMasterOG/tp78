d8f85593f2dda7e532e92a0e9a3e441fbd9ba895
QiChen <670476161@qq.com>
2022-10-06 12:44:38 +0800

    modify the battery adc range

--- mounriver_project/BLE_CH582M/HAL/include/BATTERY.h
+++ mounriver_project/BLE_CH582M/HAL/include/BATTERY.h
@@ -11,12 +11,12 @@
 
   #define ADC_MAXBUFLEN       10
 
-  #define BAT_MINADCVAL       3413  // 3.5V/2
+  #define BAT_MINADCVAL       3706  // 3.8V/2
   #define BAT_MAXADCVAL       3998  // 4.1V/2
 
-  #define BAT_25PERCENT_VAL   3559  // 25%
-  #define BAT_50PERCENT_VAL   3705  // 50%
-  #define BAT_75PERCENT_VAL   3851  // 75%
+  #define BAT_25PERCENT_VAL   (3*BAT_MINADCVAL/4+BAT_MAXADCVAL/4)  // 25%
+  #define BAT_50PERCENT_VAL   (BAT_MINADCVAL/2+BAT_MAXADCVAL/2)  // 50%
+  #define BAT_75PERCENT_VAL   (BAT_MINADCVAL/4+3*BAT_MAXADCVAL/4)  // 75%
 
   #define BAT_FLOATING_VAL    50    // ADC浮动超过该值则警告
 
